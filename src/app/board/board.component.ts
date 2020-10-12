3546ed623e88dc9370b37caa416cdbc30e1b1e6c
unknown <brodny@wp.pl>
2020-10-12 17:33:53 +0200

    Moved state to the Game component

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-board',
@@ -7,71 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoardComponent implements OnInit {
 
-  public squares: Array<string>;
+  @Input() public squares: Array<string>;
 
-  // tslint:disable-next-line: no-inferrable-types
-  public xIsNext: boolean = true;
-
-  public status: string;
+  @Output() public squareClicked: EventEmitter<number> = new EventEmitter<number>();
 
   constructor() {
-    this.squares = Array(9).fill(null);
   }
 
   ngOnInit(): void {
-    this.recalculateStatus();
   }
 
   public handleClick(square: number): void {
-    const squares = this.squares.slice();
-
-    const winner = this.calculateWinner(this.squares);
-    if (winner || this.squares[square]) {
-      return;
-    }
-
-    squares[square] = this.xIsNext ? 'X' : 'O';
-
-    this.squares = squares;
-    this.xIsNext = !this.xIsNext;
-
-    this.recalculateStatus();
-  }
-
-  private recalculateStatus(): void {
-    const winner = this.calculateWinner(this.squares);
-    let status: string;
-    if (winner) {
-      status = `Winner: ${winner}`;
-    }
-    else {
-      const nextPlayer: string = this.xIsNext ? 'X' : 'O';
-      status = `Next player: ${nextPlayer}`;
-    }
-
-    this.status = status;
-  }
-
-  private calculateWinner(squares: Array<string>): string {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-
-    // tslint:disable-next-line: prefer-for-of
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-    return null;
+    this.squareClicked.emit(square);
   }
 
 }
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -7,9 +7,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
 
+  public history: Array<Array<string>> = [ Array(9).fill(null), ];
+
+  // tslint:disable-next-line: no-inferrable-types
+  public xIsNext: boolean = true;
+
+  public status: string;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.recalculateStatus();
+  }
+
+  public handleClick(square: number): void {
+    const current: Array<string> = this.history[this.history.length - 1];
+    const squares: Array<string> = current.slice();
+
+    if (this.calculateWinner(squares) || squares[square]) {
+      return;
+    }
+
+    squares[square] = this.xIsNext ? 'X' : 'O';
+
+    this.history = this.history.concat([squares]);
+    this.xIsNext = !this.xIsNext;
+
+    this.recalculateStatus();
+  }
+
+  private calculateWinner(squares: Array<string>): string {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    // tslint:disable-next-line: prefer-for-of
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  private recalculateStatus(): void {
+    const winner = this.calculateWinner(this.history[this.history.length - 1]);
+    let status: string;
+    if (winner) {
+      status = `Winner: ${winner}`;
+    }
+    else {
+      const nextPlayer: string = this.xIsNext ? 'X' : 'O';
+      status = `Next player: ${nextPlayer}`;
+    }
+
+    this.status = status;
   }
 
 }
